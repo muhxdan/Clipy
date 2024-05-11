8fe6f7c4e5fd19e700c6c47a1268d4dbe3deca68
Danu Firdaus <danufirdaus51@gmail.com>
2024-05-12 00:03:44 +0700

    Add module supabase

--- app/src/main/java/com/salty/apps/clipy/di/SupabaseModule.kt
+++ app/src/main/java/com/salty/apps/clipy/di/SupabaseModule.kt
@@ -7,7 +7,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.gotrue.GoTrue
 import io.github.jan.supabase.postgrest.Postgrest
 import javax.inject.Singleton
 
@@ -21,7 +20,6 @@ object SupabaseModule {
             supabaseUrl = BuildConfig.SUPABASE_URL,
             supabaseKey = BuildConfig.SUPABASE_KEY
         ) {
-            install(GoTrue)
             install(Postgrest)
         }
     }
